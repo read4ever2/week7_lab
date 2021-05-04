29c82a6dd7b5d70e8de2d7aa659be974d4f19e1a
read4ever2 <62565368+read4ever2@users.noreply.github.com>
2021-05-03 21:47:03 -0500

    update

--- basic_website_v2.py
+++ basic_website_v2.py
@@ -7,8 +7,10 @@ Will Feighner
 2021 05 01
 """
 import datetime
+import string
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 
@@ -52,6 +54,75 @@ def show_register():
 def show_login():
     """Create and render login page"""
     return render_template('login.html')
+
+
+def check_pass():
+    """Checks hashed login password against user file hashed password"""
+    # Input a test password
+    test_pass = "sdev300Key!2 "
+    # Hash the password
+    hash_pass = sha256_crypt.hash(test_pass)
+    # View the encrypted password
+    print(hash_pass)
+    # Call verify to compare the two entries
+    print(sha256_crypt.verify(test_pass, hash_pass))
+
+
+def is_registered(username):
+    """Checks if user is already registered"""
+    with open('../static/pass_file.txt', "r") as pass_file:
+        if username in pass_file:
+            return True
+        return False
+
+
+def special_test(input_string, special_req):
+    """Tests if given string has enough punctuation/special characters"""
+    counter = 0
+
+    # Counts how many punctuation characters in given string
+    for char in input_string:
+        if char in string.punctuation:
+            counter += 1
+
+    return counter >= special_req
+
+
+def complexity(password):
+    """Checks password complexity"""
+    lower_case_req = 1
+    upper_case_req = 1
+    digit_count = 1
+    special_count = 1
+    length = 12
+
+    if (len(password) >= length) and (sum(char.islower() for char in password) >= lower_case_req
+                                      and sum(char.isupper() for char in password) >= upper_case_req
+                                      and sum(char.isdigit() for char in password) >= digit_count
+                                      and special_test(password, special_count)):
+        return True
+    return False
+
+
+def register(username, password, real_name, email_address):
+    """Registers user"""
+    error = ''
+
+    if not username:
+        error = 'Please enter your Username.'
+    elif not password:
+        error = 'Please enter your Password.'
+    elif not is_registered(username):
+        error = 'You are already registered'
+    elif not complexity(password):
+        error = 'Make your password more complex'
+
+    if error == '':
+        flash(error)
+
+    with open('../static/pass_file.txt', "a") as pass_file:
+        pass_file.writelines(username + " " + sha256_crypt.hash(password) + " " + real_name + " " +
+                             email_address)
 
 
 if __name__ == '__main__':
